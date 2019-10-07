8d658c5feff1609779ee0d4ea9c0ac10acc2ae97
Yahav Biran <birayaha@amazon.com>
2019-10-07 12:20:18 -0700

    adding exception handling

--- detect_text.py
+++ detect_text.py
@@ -4,11 +4,9 @@ import boto3
 
 
 def detect_text(photo, bucket):
-
-    client=boto3.client('rekognition')
-
+  client=boto3.client('rekognition')
+  try:
     response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
-                        
     textDetections=response['TextDetections']
     print ('Detected text\n----------')
     for text in textDetections:
@@ -20,10 +18,16 @@ def detect_text(photo, bucket):
             print ('Type:' + text['Type'])
             print()
     return len(textDetections)
-
+  except ThrottlingException as te:
+    print('ThrottlingException {}'.format(te))
+  except ProvisionedThroughputExceededException as pte:
+    print('ProvisionedThroughputExceededException {}'.format(pte))
+  except Exception as e:
+    print('general exception {}'.format(e))
+    
 def main():
     bucket='yahavb'
-    photo='myid.png'
+    photo='idsample.png'
     text_count=detect_text(photo,bucket)
     print("Text detected: " + str(text_count))
 
